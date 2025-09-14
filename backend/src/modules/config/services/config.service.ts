36c0cc893098e0b8b0ddb936efa6ef57e9ab1425
HydroshieldMKII <v.bureau@hotmail.com>
2025-09-14 17:43:39 -0400

    Bump app version to 1.1.3

--- backend/src/modules/config/services/config.service.ts
+++ backend/src/modules/config/services/config.service.ts
@@ -8,7 +8,7 @@ import * as http from 'http';
 import * as https from 'https';
 
 // App version
-const CURRENT_APP_VERSION = '1.1.0';
+const CURRENT_APP_VERSION = '1.1.3';
 
 export interface ConfigSettingDto {
   key: string;
