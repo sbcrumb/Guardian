767c656d8474b035be866c2eb6aa1bb89472cab1
HydroshieldMKII <v.bureau@hotmail.com>
2025-09-30 17:53:21 -0400

    fix(version): update CURRENT_APP_VERSION to 1.1.7

--- backend/src/modules/config/services/config.service.ts
+++ backend/src/modules/config/services/config.service.ts
@@ -8,7 +8,7 @@ import * as http from 'http';
 import * as https from 'https';
 
 // App version
-const CURRENT_APP_VERSION = '1.1.6';
+const CURRENT_APP_VERSION = '1.1.7';
 
 export interface ConfigSettingDto {
   key: string;
